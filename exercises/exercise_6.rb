d49c114645a4d3ac0f8530eba4bda61bcc738915
ACF <alricfernandes11@gmail.com>
2023-03-08 16:23:33 -0500

    Solving exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Brown", hourly_rate: 20)
+@store1.employees.create(first_name: "Jill", last_name: "Stevens", hourly_rate: 30)
+
+@store2.employees.create(first_name: "Maya", last_name: "Black", hourly_rate: 25)
+@store2.employees.create(first_name: "Jane", last_name: "White", hourly_rate: 35)
+@store2.employees.create(first_name: "Lizy", last_name: "Williams", hourly_rate: 45)
