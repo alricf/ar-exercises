cd05bad67d4f33d64fe1aa5a205fc1e3e3b89801
ACF <alricfernandes11@gmail.com>
2023-03-09 12:54:01 -0500

    Solving exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :must_include
+
+  def must_include
+    if !mens_apparel || !womens_apparel
+      errors.add(:must_carry, "Store must include at least one of men's apparel or women's apparel")
+    end
+  end
 end
